835bab2ed48617b62aaf7feaa0b183a9d632ac85
DuskyElf <91879372+DuskyElf@users.noreply.github.com>
2025-05-24 17:28:50 +0530

    Add: Blog site
    Add: KaTeX for Meth
    
    Ref: Font update
    
    Using JetBrains Mono for code blocks and Outfit font for blog posts to improve readability.
    
    Fix: Padding and duppliclate styles
    
    Fix: asyncronized render-blocking resources
    
    Deleted test files
    
    pure css todo app blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+import rehypeSlug from 'rehype-slug';
+import rehypeKatex from 'rehype-katex';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://duskyelf.me'
+  site: 'https://duskyelf.me',
+  integrations: [mdx()],
+
+  markdown: {
+    rehypePlugins: [rehypeKatex, rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'append' }]],
+  },
+
+  redirects: {
+    '/blog': '/blogs',
+  },
 });
